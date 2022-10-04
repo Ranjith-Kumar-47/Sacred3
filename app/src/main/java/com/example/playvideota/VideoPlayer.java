3b1dda5adf65287ed192ead8502fece9c962e86b
theamit10 <prasadak66@gmail.com>
2022-10-04 08:09:02 +0530

    REMOVED LINK FROM VIDEO PLAYER

--- app/src/main/java/com/example/playvideota/VideoPlayer.java
+++ app/src/main/java/com/example/playvideota/VideoPlayer.java
@@ -1,8 +1,11 @@
 package com.example.playvideota;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,9 +20,11 @@ import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener;
 import com.squareup.picasso.Picasso;
 
-public class VideoPlayer extends YouTubeBaseActivity {
+public class VideoPlayer extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
     String userId = "";
     String videoId = "";
@@ -30,10 +35,6 @@ public class VideoPlayer extends YouTubeBaseActivity {
     String channelIcon = "";
 
     GoogleSignInClient mGoogleSignInClient;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,53 +55,35 @@ public class VideoPlayer extends YouTubeBaseActivity {
         channelName = getIntent().getStringExtra("channelName");
         channelIcon = getIntent().getStringExtra("channelIcon");
 
+        // URL USED TO OPEN LINK
         String url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=" +userId +"&maxResults=20&q=news&type=video&videoDefinition=any&key=AIzaSyBA5stcvWxiMf5PhX6HRQJJMhC2a6ovzxo";
 
-
-        System.out.println("USERID VAl : "+userId);
-        System.out.println("VIDEOID VAL :"+videoId);
-
         TextView videoTitleT, videoDescriptionT;
-
         videoTitleT = findViewById(R.id.contentTitle);
         videoDescriptionT = findViewById(R.id.contentDescription);
-
         videoTitleT.setText(videoTitle);
         videoDescriptionT.setText(videoDescription);
 
         loadingChannelNameAndImage();
 
 
-
-
-        YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.videoPlayer);
-        youTubePlayerView.initialize(url, new YouTubePlayer.OnInitializedListener() {
+        com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView playVid = findViewById(R.id.playVid);
+        playVid.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
             @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo(videoId);
-
+            public void onReady(@NonNull com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer youTubePlayer) {
+                youTubePlayer.loadVideo(videoId,0);
                 youTubePlayer.play();
-
-            }
-
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                Toast.makeText(VideoPlayer.this, "PLayed", Toast.LENGTH_SHORT).show();
             }
         });
 
 
         Button backButton = (Button) findViewById(R.id.backButton);
-
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-
-
-
 
     }
 
@@ -113,11 +96,6 @@ public class VideoPlayer extends YouTubeBaseActivity {
 
         channelIcon = getIntent().getStringExtra("channelIcon");
         channelName = getIntent().getStringExtra("channelName");
-
-        System.out.println("Channel ICON : "+channelIcon);
-        System.out.println("Channel NAME : "+channelName);
-
-
 
         Picasso.with(getApplicationContext())
                 .load(channelIcon)
@@ -139,5 +117,16 @@ public class VideoPlayer extends YouTubeBaseActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.loadVideo(videoId);
+        youTubePlayer.play();
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
     }
 }
