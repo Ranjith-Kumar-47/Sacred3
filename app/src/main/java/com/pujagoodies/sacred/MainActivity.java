03c6cbd7e595b7d9acbe5cb3312088cfd28e0259
theamit10 <prasadak66@gmail.com>
2023-01-09 11:55:12 +0530

    added sahitya section

--- app/src/main/java/com/pujagoodies/sacred/MainActivity.java
+++ app/src/main/java/com/pujagoodies/sacred/MainActivity.java
@@ -69,15 +69,15 @@ public class MainActivity extends AppCompatActivity {
 
     ViewPager2 viewPager;
     TabLayout tabLayout;
-//    private String[] titles = {"TV", "PANCHANG", "RASHIPHAL", "GEETA SLOK", "SAHITYA"};
-    private String[] titles = {"TV", "PANCHANG", "RASHIPHAL", "GEETA SLOK"};
+    private String[] titles = {"TV", "PANCHANG", "RASHIPHAL", "GEETA SLOK", "SAHITYA"};
+//    private String[] titles = {"TV", "PANCHANG", "RASHIPHAL", "GEETA SLOK"};
 
     private int[] tabIcons = {
             R.drawable.tv_icon_btn,
             R.drawable.ic_calender_day_love_svgrepo_com,
             R.drawable.astrology,
             R.drawable.gita_image,
-//            R.drawable.gita_slok_icon
+            R.drawable.gita_slok_icon
     };
     ViewPagerAdapter viewPagerAdapter;
 
@@ -160,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(1).setText("पंचांग").setIcon(R.drawable.ic_baseline_calendar_month_24).setTabLabelVisibility(TabLayout.TAB_LABEL_VISIBILITY_LABELED);
         tabLayout.getTabAt(2).setText("राशि").setIcon(R.drawable.astrology);
         tabLayout.getTabAt(3).setText("गीता श्लोक").setIcon(R.drawable.gita_image);
-//        tabLayout.getTabAt(4).setText("साहित्य").setIcon(R.drawable.gita_slok_icon);
+        tabLayout.getTabAt(4).setText("साहित्य").setIcon(R.drawable.gita_slok_icon);
 
 
 //        // setting the root fragment for home page
